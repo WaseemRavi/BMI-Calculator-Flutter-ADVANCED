17978bcd045e18b70ade795bb987c8e2cdaaa9c4
smama <smamahsn@gmail.com>
2020-11-25 22:44:37 +0500

    Second page color changed

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 
 const kBottomContainerHeight = 80.0;
-const kActiveCardColour = Color(0xFF1D1E33);
-const kInactiveCardColour = Color(0xFF111328);
+const kActiveCardColour = Colors.yellowAccent;
+const kInactiveCardColour = Colors.indigoAccent;
 const kBottomContainerColour = Color(0xFFEB1555);
 
 const kLabelTextStyle = TextStyle(
